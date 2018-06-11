c3707ba7228e3643f56e06e4d64d0b3256b29db1
Jared Norman <jared@super.gd>
2018-06-11 11:10:19 -0700

    Fix no arrows

--- keyboards/ergodox_ez/keymaps/creepywizard/keymap.c
+++ keyboards/ergodox_ez/keymaps/creepywizard/keymap.c
@@ -16,23 +16,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,         KC_R,   KC_T,   KC_LCBR,
         KC_SESC, KC_A,    KC_S,    KC_D,         KC_F,   KC_G,
         KC_LSPO, KC_Z,    KC_X,    KC_C,         KC_V,   KC_B,   KC_LABK,
-        KC_LCTL, KC_LALT, KC_LGUI, KC_NO,        KC_NO,
+        KC_LCTL, KC_LALT, KC_LGUI, KC_VOLU,      KC_VOLD,
 
-                                                         KC_MPLY,KC_VOLU,
-                                                                 KC_VOLD,
-                                                 KC_SPC, KC_ENT, LGUI(KC_V),
+                                                         KC_LEFT,KC_RGHT,
+                                                                 KC_UP,
+                                                 KC_SPC, KC_ENT, KC_DOWN,
 
         // Right hand:
         KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,      KC_EQL,
         KC_RCBR, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,      KC_MINS,
                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCOLON, KC_QUOT,
         KC_RABK, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RSPC,
-                          KC_NO,   KC_LBRC, KC_RBRC, KC_BSLS,   TG(NORMAN),
+                        TG(NORMAN),KC_LBRC, KC_RBRC, KC_BSLS,   KC_MPLY,
 
         KC_HOME, KC_END,
         KC_PGUP,
-        KC_PGDN, KC_TRNS, KC_BSPC
-// FIXME I've got no arrows now!!!
+        KC_PGDN, LGUI(KC_V), KC_BSPC
     ),
     [NORMAN] = KEYMAP(
        // Left hand:

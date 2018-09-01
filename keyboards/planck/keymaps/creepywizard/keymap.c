e4a4bf9621c4b2714715f9e6e8cf0c4d33f444db
Jared Norman <jared@super.gd>
2018-08-31 22:03:24 -0700

    Fresh planck layout

--- keyboards/planck/keymaps/creepywizard/keymap.c
+++ keyboards/planck/keymaps/creepywizard/keymap.c
@@ -1,9 +1,6 @@
 #include "planck.h"
-#include "eeconfig.h"
-#include "audio.h"
-
-float tone_startup[][2] = SONG(
-);
+#include "action_layer.h"
+#include "muse.h"
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -11,86 +8,74 @@ float tone_startup[][2] = SONG(
 // entirely and just use numbers.
 
 // Fillers to make layering more clear
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
+#define QWERTY 0
+#define NORMAN 1
+#define MOD 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+/* QWERTY
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |   _  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |   '  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Play | Ctrl | Mod  | Alt  |Lower |Space |Enter |Backsp|   [  |   ]  |   \  |Backsp|
+ * `-----------------------------------------------------------------------------------'
+ */
+[QWERTY] = LAYOUT_planck_grid(
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+    KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, MO(MOD), KC_SPC,  KC_ENT,  KC_BSPC, KC_LBRC, KC_RBRC, KC_BSLS, KC_MPLY
+),
+
 /* Norman
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   D  |   F  |   K  |   J  |   U  |   R  |   L  |   ;  |   _  |
+ * |      |   Q  |   W  |   D  |   F  |   K  |   J  |   U  |   R  |   L  |   ;  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   E  |   T  |   G  |   Y  |   N  |   I  |   O  |   H  |   '  |
+ * |      |   A  |   S  |   E  |   T  |   G  |   Y  |   N  |   I  |   O  |   H  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   P  |   M  |   ,  |   .  |   /  | Shift|
+ * |      |   Z  |   X  |   C  |   V  |   B  |   P  |   M  |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Play | Ctrl | Mod  | Alt  |Lower |Space |Enter |Raise |   [  |   ]  |   \  |Backsp|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[0] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,    KC_J,    KC_U,    KC_R,    KC_L,    KC_SCLN, KC_MINS},
-  {KC_ESC,  KC_A,    KC_S,    KC_E,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_I,    KC_O,    KC_H,    KC_QUOT},
-  {KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC},
-  {KC_MPLY, KC_LCTL, KC_LGUI, KC_LALT, MO(1),   KC_SPC,  KC_ENT,  MO(2),   KC_LBRC, KC_RBRC, KC_BSLS, KC_BSPC}
-},
+[NORMAN] = LAYOUT_planck_grid(
+    _______, KC_Q,    KC_W,    KC_D,    KC_F,    KC_K,    KC_J,    KC_U,    KC_R,    KC_L,    KC_SCLN, _______,
+    _______, KC_A,    KC_S,    KC_E,    KC_T,    KC_G,    KC_Y,    KC_N,    KC_I,    KC_O,    KC_H,    _______,
+    _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P,    KC_M,    _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
 
-/* Lower
+/* Mod
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |      |      |      |      |      |      |      |      |      |      |   +  |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |   +  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |   =  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   =  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |Trans | Left | Down |  Up  |Right |      |      |      |      |      |      |Trans |
+ * |Trans | Left | Down |  Up  |Right | Vol- | Vol+ | Home | PgDn | PgUp | End  |Trans |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |Trans |Trans |Trans |Trans |Trans |Trans |Trans |Trans |      |      |      |Trans |
  * `-----------------------------------------------------------------------------------'
  */
-[1] = {
-  {KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PLUS},
-  {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL},
-  {_______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______}
-},
-
-/* Raise
- * ,-----------------------------------------------------------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Vol+ |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |Trans | Home | PgDn | PgUp | End  |      |      |      |      |      |      | Vol- |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Trans |Trans |Trans |Trans |Trans |Trans |Trans |Trans |      |      |      |Trans |
- * `-----------------------------------------------------------------------------------'
- */
-[2] = {
-  {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12},
-  {XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_VOLU},
-  {_______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD},
-  {_______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, _______}
-},
+[MOD] = LAYOUT_planck_grid(
+    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PLUS,
+    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_EQL ,
+    _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_VOLD, KC_VOLU, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, TG(NORMAN)
+)
 
 };
 
 void matrix_init_user(void) {
-   startup_user();
-}
-
-void startup_user() {
-    _delay_ms(20); // gets rid of tick
-    PLAY_NOTE_ARRAY(tone_startup, false, 0);
 }
 
 const uint16_t PROGMEM fn_actions[] = {
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
-  switch(id) {
-    case 1:
-      if (record->event.pressed) {
-        PLAY_NOTE_ARRAY(tone_startup, false, 0);
-      }
-      break;
-  }
   return MACRO_NONE;
 };
